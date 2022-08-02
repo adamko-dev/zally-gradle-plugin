bf7e80503bd7f7fd13f554c16077430ebaa4c299
Adam <897017+aSemy@users.noreply.github.com>
2022-08-03 00:10:49 +0200

    add small note about Gradle plugin marker

--- plugin-marker/build.gradle.kts
+++ plugin-marker/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
   `maven-publish`
 }
 
+// custom Gradle Plugin Marker, to workaround Jitpack overriding the group
+// https://docs.gradle.org/current/userguide/plugins.html#sec:plugin_markers
+
 group = "com.github.adamko-dev.zally-gradle-plugin"
 version = "0.0.1-SNAPSHOT"
 
